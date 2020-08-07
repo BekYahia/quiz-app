cc62c6105b8357aa1076a17052f29c3886fd2ec8
BekYahia <albakrieee@gmail.com>
2020-08-07 20:47:48 +0200

    prevent direct access to /setup page

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ const routes = [
     {
         path: "/setup",
         component: Setup,
+        redirect: { name: "StepOne" },
         children: [
             {
                 path: "step-one",
